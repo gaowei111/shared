9a662ce2796923787c842ee38bcf0a2a8b2f9c18
jofgao <joffery.gao@adidas.com>
2024-11-20 18:07:30 +0800

    init

---
+++ Package.swift
@@ -0,0 +1,23 @@
+// swift-tools-version: 5.10
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "kmpSharedIos",
+    products: [
+        // Products define the executables and libraries a package produces, making them visible to other packages.
+        .library(
+            name: "kmpSharedIos",
+            targets: ["kmpSharedIos"]),
+    ],
+    targets: [
+        // Targets are the basic building blocks of a package, defining a module or a test suite.
+        // Targets can depend on other targets in this package and products from dependencies.
+        .binaryTarget(
+            name: "kmpSharedIos",
+            url: "https://artifactory.tools.adidas.com.cn/artifactory/Flagship-android/cn/adidas/kmp/XCFramework/0.0.7/sharedKMP.xcframework.zip",
+            checksum: "14e95548eeb0d7de814881dbc138bbed4bcee007f81331f3aee1912ca31b68d4"
+        )
+    ]
+)
